7b2b28d7503fd222dbb7b6edeea5133eb12515b7
Einstein Villamor <einsteinvillamor@gmail.com>
2017-04-01 23:48:36 -0700

    memoized fibonacci

---
+++ fib_memoized.js
@@ -0,0 +1,16 @@
+function fib(num, memo){
+  if(num <= 0) { return 0;}
+  if(num <= 2) { return 1;}
+
+  //memoized
+  if(memo[num - 1] === undefined){
+    memo[num - 1] = fib(num - 1, memo);
+  }
+
+  //memoized
+  if(memo[num - 2] === undefined){
+    memo[num - 2] = fib(num - 2, memo);
+  }
+
+  return memo[num - 1] + memo[num -2];
+}
